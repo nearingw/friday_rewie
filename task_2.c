1baff898cfb74cfa76631b179ca34dff0d559068
nearingw <166845542+nearingw@users.noreply.github.com>
2024-04-12 20:10:22 +0300

    Update task_2.c

--- task_2.c
+++ task_2.c
@@ -5,7 +5,7 @@
 int main() {
   int p = 21500;
   double r;
-    if (scanf("%lf", &r) == 1) {
+    if (scanf("%lf", &r) == 1 && r>=0) {
       double v = (4.0 / 3.0) * M_PI * pow(r, 3.0);
       double m = p * v;
       double result = round(m);
