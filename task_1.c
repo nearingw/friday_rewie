cf6cbb010a9b954df7f0fa617619fde09a7a53fe
nearingw <166845542+nearingw@users.noreply.github.com>
2024-04-12 20:16:56 +0300

    Update task_1.c

--- task_1.c
+++ task_1.c
@@ -1,43 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main () {
-    int N;
-    scanf ("%d", &N);
-    int A [N][N];
-    int B [N][N];
-    int C [N][N];
-    
-    for (int i = 0; i<N; i++) {
-        for (int j = 0; j<N; j++) {
-            scanf ("%d", &A[i][j]);
-        }
-    }
+int main() {
+  int N;
+  scanf("%d", &N);
+  int A[N][N];
+  int B[N][N];
+  int C[N][N];
 
-    for (int i = 0; i<N; i++) {
-        for (int j = 0; j<N; j++) {
-            scanf ("%d", &B[i][j]);
-        }
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      scanf("%d", &A[i][j]);
     }
+  }
 
-    for (int i = 0; i<N; i++) {
-        for (int j = 0; j<N; j++) {
-            C[i][j] = 0;
-            for (int c = 0; c<N; c++) {
-                C[i][j] += A[i][c] * B[c][j];
-            }
-        }
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      scanf("%d", &B[i][j]);
     }
+  }
 
-
-    for (int i = 0; i<N; i++) {
-        for (int j = 0; j<N; j++) {
-            if (j == N-1) printf ("%d", C[i][j]);
-            else printf ("%d ", C[i][j]);
-        }
-        if (i!=N-1) printf ("\n");
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      C[i][j] = 0;
+      for (int c = 0; c < N; c++) {
+        C[i][j] += A[i][c] * B[c][j];
+      }
     }
-    return 0;
+  }
+
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      if (j == N - 1)
+        printf("%d", C[i][j]);
+      else
+        printf("%d ", C[i][j]);
+    }
+    if (i != N - 1) printf("\n");
+  }
+  return 0;
 }
-
-
